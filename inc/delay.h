bbeae7561e9c63dcc61d118af7a8710887ba2f81
Aaron Perl <aaronjperl@gmail.com>
2017-12-02 15:36:10 -0500

    Implemented timer functionality for delay purposes

---
+++ inc/delay.h
@@ -0,0 +1,16 @@
+/*
+ * delay.h
+ *
+ *  Created on: Dec 2, 2017
+ *      Author: Aaron
+ */
+
+#ifndef INC_DELAY_H_
+#define INC_DELAY_H_
+
+#include <stdint.h>
+
+uint32_t getMillis();
+void resetMillis();
+
+#endif /* INC_DELAY_H_ */
---
+++ src/delay.c
@@ -0,0 +1,22 @@
+#include "SI_EFM8UB1_Register_Enums.h"
+#include "delay.h"
+
+static volatile uint32_t millis = 0;
+
+uint32_t getMillis()
+{
+  return millis;
+}
+
+void resetMillis()
+{
+  millis = 0;
+}
+
+SI_INTERRUPT(timer2ISR, TIMER2_IRQn)
+{
+  // Increment millisecond counter
+  millis++;
+  // Clear interrupt flag
+  TMR2CN0 &= ~(TMR2CN0_TF2H__SET | TMR2CN0_TF2L__SET);
+}
