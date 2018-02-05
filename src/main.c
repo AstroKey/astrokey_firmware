b50e517b6a6888aec725e189ceb5f52124a2579e
Aaron Perl <aaronjperl@gmail.com>
2018-02-04 20:57:13 -0500

    Updated comments in main.c file

--- src/main.c
+++ src/main.c
@@ -1,48 +1,16 @@
 //-----------------------------------------------------------------------------
 // main.c
 //-----------------------------------------------------------------------------
-// Copyri999ght 2014 Silicon Laboratories, Inc.
-// http://developer.silabs.com/legal/version/v11/Silicon_Labs_Software_License_Agreement.txt
+// Copyright 2018 AstroKey
+// https://github.com/AstroKey/astrokey_firmware/blob/master/LICENSE
 //
 // Program Description:
 //
-// This program enumerates as a USB keyboard. Each time a button is pressed
-// a character is sent to the host. A series of presses will spell out
-// "HID Keyboard ". The status of the Caps Lock and Num Lock on the host will
-// be indicated by the color of the LED.
-//
-// Resources:
-// SYSCLK - 48 MHz HFOSC1 / 1
-// USB0   - Full speed
-// P0.2 - push button
-// P0.3 - push button
-// P2.3 - Display enable
-//
-//-----------------------------------------------------------------------------
-// How To Test: EFM8UB1 STK
-//-----------------------------------------------------------------------------
-// 1) Place the switch in "AEM" mode.
-// 2) Connect the EFM8UB1 STK board to a PC using a mini USB cable.
-// 3) Compile and download code to the EFM8UB1 STK board.
-//    In Simplicity Studio IDE, select Run -> Debug from the menu bar,
-//    click the Debug button in the quick menu, or press F11.
-// 4) Run the code.
-//    In Simplicity Studio IDE, select Run -> Resume from the menu bar,
-//    click the Resume button in the quick menu, or press F8.
-// 5) The HID keyboard demo should start.
-// 6) Connect a micro USB cable from the PC to the STK.
-// 7) The device should enumerate on the PC as a HID keyboard.
-// 8) Press either push-button (PB0 or PB1) to send one character in the
-//    string "HID Keyboard ".
-// 9) Pressing Caps Lock or Num Lock on the host keyboard will change the color
-//    of the LED.
-//
-// Target:         EFM8UB1
-// Tool chain:     Generic
-//
-// Release 0.1 (JM)
-//    - Initial Revision
-//    - 26 JAN 2015
+// AstroKey is an open-source platform for automating keyboard workflows.
+// This program is the firmware running on the microcontroller found in AstroKey.
+// It enumerates as a USB device with 2 interfaces, 1 to implement WebUSB and the other a HID.
+// The WebUSB Interface allows the AstroKey website to save workflows created by the user to the device.
+// The HID Interface plays back macros.
 //
 #include "SI_EFM8UB1_Register_Enums.h"
 #include "astrokey.h"
@@ -53,12 +21,7 @@
 // main() Routine
 // ---------------------------------------------------------------------------
 //
-// Note: The software watchdog timer is not disabled by default in this
-// example, so a long-running program will reset periodically unless
-// the timer is disabled or your program periodically writes to it.
-//
-// Review the "Watchdog Timer" section under the part family's datasheet
-// for details.
+// Initializes AstroKey functionality and polls the device inputs.
 //
 // ----------------------------------------------------------------------------
 int16_t main(void)
